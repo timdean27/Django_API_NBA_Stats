7bfdec9c32ed8b029e02c22eef01f352fca44a71
timdean27 <timdean27@yahoo.com>
2024-08-20 13:23:07 -0400

    updated models views and serializer to have class fro players and nested classes for season statesa and 5 day stats

--- nba_stats_api/stats_api_app/models.py
+++ nba_stats_api/stats_api_app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.team})"
+
+
+class Player_Season_stats_model(models.Model):
+    player = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
     season_average_games_played = models.IntegerField() 
     season_average_minutes_per_game = models.FloatField()  
     season_average_points_per_game = models.FloatField()  
@@ -26,7 +33,37 @@ class Player(models.Model):
     season_average_fantasy_points = models.FloatField() 
     season_average_double_doubles = models.FloatField()  
     season_average_triple_doubles = models.FloatField()  
-    season_average_plus_minus = models.FloatField()  
+    season_average_plus_minus = models.FloatField()
 
     def __str__(self):
-        return f"{self.name} ({self.team})"
+        return f"{self.player.name} ({self.player.team})"
+
+
+class Player_LastFive_stats_model(models.Model):
+    player = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
+    last_five_minutes_per_game = models.JSONField()  
+    last_five_points_per_game = models.JSONField()   
+    last_five_field_goals_made = models.JSONField()  
+    last_five_field_goals_attempted = models.JSONField()  
+    last_five_field_goal_percentage = models.JSONField()  
+    last_five_three_points_made = models.JSONField()  
+    last_five_three_points_attempted = models.JSONField()  
+    last_five_three_point_percentage = models.JSONField()  
+    last_five_free_throws_made = models.JSONField()  
+    last_five_free_throws_attempted = models.JSONField() 
+    last_five_free_throw_percentage = models.JSONField()  
+    last_five_offensive_rebounds = models.JSONField()  
+    last_five_defensive_rebounds = models.JSONField()
+    last_five_total_rebounds = models.JSONField()
+    last_five_assists = models.JSONField()
+    last_five_turnovers = models.JSONField()
+    last_five_steals = models.JSONField()  
+    last_five_blocks = models.JSONField()  
+    last_five_personal_fouls = models.JSONField()
+    last_five_fantasy_points = models.JSONField()
+    last_five_double_doubles = models.JSONField()  
+    last_five_triple_doubles = models.JSONField()  
+    last_five_plus_minus = models.JSONField()
+
+    def __str__(self):
+        return f"{self.player.name} ({self.player.team})"
--- nba_stats_api/stats_api_app/serializers.py
+++ nba_stats_api/stats_api_app/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Player
+from .models import Player, Player_Season_stats_model, Player_LastFive_stats_model
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
+        fields = ['id', 'name', 'team']
+
+class PlayerSeasonStatsSerializer(serializers.ModelSerializer):
+    player = PlayerSerializer()  # Nested serializer
+
+    class Meta:
+        model = Player_Season_stats_model
+        fields = '__all__'
+
+class PlayerLastFiveStatsSerializer(serializers.ModelSerializer):
+    player = PlayerSerializer()  # Nested serializer
+
+    class Meta:
+        model = Player_LastFive_stats_model
         fields = '__all__'
