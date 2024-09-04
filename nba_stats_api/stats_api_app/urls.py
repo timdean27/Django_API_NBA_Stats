c433c9af63f27c60e8f3294510ddba2c78576e82
timdean27 <timdean27@yahoo.com>
2024-09-03 20:41:11 -0400

    changed views and urls to work with player_id and then changes pk to id, will add new views to find /season/player_id/id , this should only get() 1 and allow to updated and delete

--- nba_stats_api/stats_api_app/urls.py
+++ nba_stats_api/stats_api_app/urls.py
@@ -4,6 +4,8 @@ from .views import player_list, player_season_details, player_last_five_details,
 urlpatterns = [
     path('', home, name='home'),
     path('api/players/', player_list, name='player-list'),
-    path('api/player/season/<int:pk>/', player_season_details, name='player-season-details'),
-    path('api/player/last5/<int:pk>/', player_last_five_details, name='player-last-five-details'),
+    path('api/player/season/<int:player_id>/', player_season_details, name='player-season-details'),
+    path('api/player/season/<int:player_id>/<int:id>', player_season_details, name='player-season-details'),
+    path('api/player/last5/<int:player_id>/', player_last_five_details, name='player-last-five-details'),
+    path('api/player/last5/<int:player_id>/<int:id>', player_last_five_details, name='player-last-five-details'),
 ]
