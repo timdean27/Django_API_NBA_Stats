4aeb4e64e87f33bee0f900c5cd88e534b7ab27a7
timdean27 <timdean27@yahoo.com>
2024-09-03 20:55:28 -0400

    created new views and urls for getting season of player by player_id/id and same for last5 game , can get updated and delete , get , put ,delete

--- nba_stats_api/stats_api_app/urls.py
+++ nba_stats_api/stats_api_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import player_list, player_season_details, player_last_five_details, home
+from .views import home, player_list, player_season_details, single_player_season_details, player_last_five_details, single_player_last_five_details
 
 urlpatterns = [
     path('', home, name='home'),
     path('api/players/', player_list, name='player-list'),
-    path('api/player/season/<int:player_id>/', player_season_details, name='player-season-details'),
-    path('api/player/season/<int:player_id>/<int:id>', player_season_details, name='player-season-details'),
-    path('api/player/last5/<int:player_id>/', player_last_five_details, name='player-last-five-details'),
-    path('api/player/last5/<int:player_id>/<int:id>', player_last_five_details, name='player-last-five-details'),
+    path('api/player/season/<int:player_id>/', player_season_details, name='player-season-list'),
+    path('api/player/season/<int:player_id>/<int:id>/', single_player_season_details, name='single_player_season_details'),
+    path('api/player/last5/<int:player_id>/', player_last_five_details, name='player-last-five-list'),
+    path('api/player/last5/<int:player_id>/<int:id>/', single_player_last_five_details, name='single_player_last_five_details'),
 ]
--- nba_stats_api/stats_api_app/views.py
+++ nba_stats_api/stats_api_app/views.py
@@ -35,6 +35,28 @@ def player_season_details(request, player_id):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def single_player_season_details(request, player_id, id):
+    try:
+        player_stats = NBAPlayerSeasonStats.objects.get(id=id, player_id=player_id)
+    except NBAPlayerSeasonStats.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NBAPlayerSeasonStatsSerializer(player_stats)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = NBAPlayerSeasonStatsSerializer(player_stats, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        player_stats.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'POST'])
 def player_last_five_details(request, player_id):
     if request.method == 'GET':
@@ -48,3 +70,25 @@ def player_last_five_details(request, player_id):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def single_player_last_five_details(request, player_id, id):
+    try:
+        player_stats = NBAPlayersLast5Games.objects.get(id=id, player_id=player_id)
+    except NBAPlayersLast5Games.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NBAPlayersLast5GamesSerializer(player_stats)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = NBAPlayersLast5GamesSerializer(player_stats, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        player_stats.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
